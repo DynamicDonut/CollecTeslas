ff0fb55b7b43dbda500a98d35ea2f2c2b5869fd4
Syrantking <skullomania51@gmail.com>
2016-01-17 17:02:55 -0500

    Jan17
    Balls

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,69 +5,104 @@ public class PlayerMovement : MonoBehaviour {
 	public float speed;
 
 	Vector3 move;
-	public Animator anim;
+	public Animator get;
 	public bool faceUp;
 	public bool faceDown;
 	public bool faceLeft;
 	public bool faceRight;
 	bool buttonPress = false;
+	public int lockPos = 0;
+
+
 	// Use this for initialization
 	void Start () {
-		anim = GetComponent<Animator>();
-		anim.Play ("Player_IdleD", -1);
+		get = GetComponent<Animator>();
+		get.Play ("Player_IdleD", -1);
+
+
+	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, lockPos, lockPos);
 
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			anim.Play ("Player_WalkU", -1);
+		if (Input.GetKey (KeyCode.UpArrow) && !buttonPress) {
+			get.Play ("Player_WalkU", -1);
 			move = Vector3.up;
 			faceUp = true;
 			faceDown = false;
 			faceRight = false;
 			faceLeft = false;
-		} else if (faceUp) {
-			anim.Play ("Player_IdleU", -1);
+			buttonPress = true;
+		} 
+		else if (faceUp  && Input.GetKeyUp(KeyCode.UpArrow)) 
+		{
+			get.Play ("Player_IdleU", -1);
+			buttonPress = false;
 		}
 		
-		if (Input.GetKey (KeyCode.DownArrow))
+		if (Input.GetKey (KeyCode.DownArrow)  && !buttonPress)
 		{
-			anim.Play ("Player_WalkD", -1);
+			get.Play ("Player_WalkD", -1);
 			move = Vector3.down;
 			faceUp = false;
 			faceDown = true;
 			faceRight = false;
 			faceLeft = false;
+			buttonPress = true;
 
-		}else if (faceDown) {
-			anim.Play ("Player_IdleD", -1);
+		}
+		else if (faceDown && Input.GetKeyUp(KeyCode.DownArrow)) 
+		{
+			get.Play ("Player_IdleD", -1);
+			buttonPress = false;
 		}
 				
 
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			anim.Play ("Player_WalkL", -1);
+		if (Input.GetKey (KeyCode.LeftArrow) && !buttonPress) {
+			get.Play ("Player_WalkL", -1);
 			move = Vector3.left;
 			faceUp = false;
 			faceDown = false;
 			faceRight = false;
 			faceLeft = true;
-		} else if (faceLeft) {
-			anim.Play ("Player_IdleL", -1);		
+			buttonPress = true;
+
+		} else if (faceLeft && Input.GetKeyUp(KeyCode.LeftArrow)) {
+			get.Play ("Player_IdleL", -1);	
+			buttonPress = false;
 		}
 			
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			anim.Play ("Player_WalkR", -1);
+		if (Input.GetKey (KeyCode.RightArrow) && !buttonPress) {
+			get.Play ("Player_WalkR", -1);
 			move = Vector3.right;
 			faceUp = false;
 			faceDown = false;
 			faceRight = true;
 			faceLeft = false;
-		} else if (faceRight) {
-			anim.Play ("Player_IdleR", -1);	
+			buttonPress = true;
+		} else if (faceRight && Input.GetKeyUp(KeyCode.RightArrow))
+		{
+			get.Play ("Player_IdleR", -1);	
+			buttonPress = false;
 		}
+
 
 		move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
 		transform.position += move * speed * Time.deltaTime;
+
+
 	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		if (other.tag == "Items") {
+			Destroy (other.gameObject);
+			speed = speed * 2;
+		}
+	}
+
+
 }
